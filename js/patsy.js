cda9cf9ebe5c2e278e1f19086a42e3ba33b75e24
Garann Means <garann@gmail.com>
2012-02-18 22:25:09 -0600

    loading sort of working, template rendering is acting bananas

--- js/patsy.js
+++ js/patsy.js
@@ -7,40 +7,45 @@ var patsy = {
 		this.currentDir = dir;
 		this.commandLine;
 
-		this.prompt.on( "keydown", function( e ) {
-			var txt = that.commandLine.text(),
-				cmd,
-				dirs;
-			if ( e.keyCode != 13 ) {
-				that.commandLine.text( txt + e.keyCode );
-			} else {
-				cmd = txt.substring( 0, txt.indexOf( " " ) );
-				if ( cmd == "ls" ) {
-					require( ["text!" + dir + "/index.tmpl"], function( tmpl) {
-						that.render( patsy.getHtml( tmpl ) );
-					});
-				} else if ( cmd == "cd" ) {
-					txt = txt.replace( "cd " ).split( "/" );
-					dirs = this.currentDir.split( "/" );
-					for ( var i=0; i<txt.length; i++ ) {
-						if ( txt[i] == ".." ) {
-							dirs.pop();
-						} else {
-							dirs.push( txt[i] );
-						}
-					}
-					this.currentDir = dirs.join( "/" );
+		$( document ).on( "keypress", function( e ) {
+			if ( that.commandLine ) {
+				var txt = that.commandLine.text(),
+					cmd,
+					dirs;
+				if ( e.keyCode != 13 ) {
+					that.commandLine.text( txt + String.fromCharCode( e.keyCode ) );
 				} else {
-					txt = txt.replace( cmd, "" );
-					require( ["text!" + that.currentDir + "/" + cmd + ".tmpl"], function( tmpl ) {
-						that.render( txt.length ?
-							patsy.getHtml( tmpl, txt ) :
-							patsy.getHtml( tmpl )			
-						);
-					});
+					cmd = txt.indexOf( " " ) > -1 ? txt.substring( 0,  txt.indexOf( " " ) ) : txt;
+					if ( cmd == "ls" ) {
+						require( ["text!" + dir + "/index.tmpl"], function( tmpl) {
+							that.render( patsy.getHtml( tmpl ) );
+						});
+					} else if ( cmd == "cd" ) {
+						txt = txt.replace( "cd " ).split( "/" );
+						dirs = this.currentDir.split( "/" );
+						for ( var i=0; i<txt.length; i++ ) {
+							if ( txt[i] == ".." ) {
+								dirs.pop();
+							} else {
+								dirs.push( txt[i] );
+							}
+						}
+						this.currentDir = dirs.join( "/" );
+					} else {
+						txt = txt.replace( cmd, "" );
+						require( ["text!" + that.currentDir + "/" + cmd + ".tmpl"], function( tmpl ) {
+							that.render( txt.length ?
+								patsy.getHtml( tmpl, txt ) :
+								patsy.getHtml( tmpl )			
+							);
+						});
+					}
 				}
 			}
+		});
 
+		this.prompt.on( "click", function() {
+			that.commandLine = $( this ).find( "span.patsy-cli" );
 		});
 
 		this.render = function( html ) {
@@ -62,13 +67,13 @@ var patsy = {
 		        var matches = txt.substr(position).match(/<(\/)?([^ \/>]*).*?(\/)?>|./),
 		            html = output.html(),
 		            ending;
-		        if ( matches[2] && !matches[3] ) {
-		            ending = closing.length ? html.lastIndexOf(closing) : -1;
-	                closing = "</" + matches[2] + ">" + closing;
-	            } else if ( matches[1] ) {
+		        if ( matches[1] ) {
 	            	ending = closing.length ? html.lastIndexOf(closing) : -1;
 		            closing = closing.replace(("</" + matches[2] + ">"),"");
-		        } else {
+		        } else if ( matches[2] && !matches[3] ) {
+		            ending = closing.length ? html.lastIndexOf(closing) : -1;
+	                closing = "</" + matches[2] + ">" + closing;
+	            } else {
 		            ending = html.lastIndexOf(closing);
 		        }
 		        p += matches[0].length;
@@ -81,9 +86,10 @@ var patsy = {
 	},
 	getHtml: function( tmpl, data ) {
 		if ( tmpl ) {
+			console.log(tmpl)
 			return doT.template( tmpl )( data || {} );
 		} else {
-			return '<div class="patsy-prompt">$ <span class="patsy-cli"></span></div>';
+			return '<div class="patsy-prompt">$ <span class="patsy-cli"></span>▒</div>';
 		}
 	}
 
